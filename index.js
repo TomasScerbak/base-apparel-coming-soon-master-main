34f33d0148f9a1b85a9a36441a61548c46750f80
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-03-17 16:29:09 +0100

    adding if statment for empty email value

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 let form = document.getElementById('form');
 let input = document.querySelector('input');
-let email = document.getElementById('email').value;
+let email = document.getElementById('email-email');
 let errorIcon = document.getElementById('error-icon');
 let error = document.getElementById('error-text');
 let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
 
-form.addEventListener('keydown', function() {
-
-    if (email.match(pattern)) {
-
+email.addEventListener('keydown', function() {
+    if (email.value.match(pattern)) {
+        
         errorIcon.classList.add('hidden');
         error.innerText = 'Your address is valid'
         error.style.color = '#62fc03';
 
+
+    } else if (email.value === '' || email.value === 'undifined' || email.value === null) {
+        errorIcon.classList.add('hidden');
+        error.innerText = '';
     } else {
 
         errorIcon.classList.remove('hidden');
         error.classList.remove('hidden');
         error.innerText = 'Please enter valid email address';
-
+        error.style.color = 'red';
     }
 })
 
