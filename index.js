b76f98e9838884be030f29241574c703f2ec40c4
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-03-17 15:38:57 +0100

    fixing bug with imput

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let form = document.getElementById('form');
+let input = document.querySelector('input');
 let email = document.getElementById('email').value;
 let errorIcon = document.getElementById('error-icon');
 let error = document.getElementById('error-text');
@@ -6,18 +7,22 @@ let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
 
 form.addEventListener('keydown', function() {
-    if (!email.match(pattern)) {
 
-        errorIcon.classList.remove('hidden');
-        error.classList.remove('hidden');
-        error.innerText = 'Please enter valid email address'
-
-    } else {
+    if (email.match(pattern)) {
 
         errorIcon.classList.add('hidden');
-        error.classList.add('hidden');
         error.innerText = 'Your address is valid'
         error.style.color = '#62fc03';
 
+    } else {
+
+        errorIcon.classList.remove('hidden');
+        error.classList.remove('hidden');
+        error.innerText = 'Please enter valid email address';
+
     }
+})
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
 })
